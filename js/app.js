d469137acf8a71737da490553ccde856b16c0d29
prof. Marnei Cardoso <marneicardoso.geracaotech@gmail.com>
2026-03-20 11:27:45 -0300

    Estrutura básica

--- js/app.js
+++ js/app.js
@@ -7,6 +7,9 @@ formCadastro.addEventListener("submit", (envio) => {
     envio.preventDefault();
 
     alert("Cadastro efetuado com sucesso!");
+
+    // Limpa os campos do form
+    formCadastro.reset();
 });
 
 // Guarda as informações do input do CEP
@@ -33,12 +36,37 @@ function buscarCEP() {
         .then(response => response.json())
         
         // Mostra os dados
-        .then(dados => {
-            console.log(dados)
+        .then((dados) => {
+            if (dados.erro) {
+                console.error("CEP inválido: ", dados.erro);
+
+                // Limpa os campos do Endereço
+                formCadastro.logradouro.value = "";
+                formCadastro.bairro.value = "";
+                formCadastro.cidade.value = "";
+                formCadastro.uf.value = "";
+                formCadastro.numero.value = "";
+                formCadastro.complemento.value = "";
+
+                return;
+            }
+
+            // Preenche os campos do form
+            formCadastro.logradouro.value = dados.logradouro;
+            formCadastro.bairro.value = dados.bairro;
+            formCadastro.cidade.value = dados.localidade;
+            formCadastro.uf.value = dados.uf;
+
+            // Coloca o foco no campo Número
+            formCadastro.numero.focus();
         })
         
         // Se houver erro, captura
-        .catch(erro => {
-            console.error("CEP inválido");
-        });
+        // .catch(erro => {
+        //     console.error("CEP inválido: ", erro);
+        // });
+
+        // * ATENÇÃO: a API ViaCEP não tem um comportamento específico
+        // para quando o CEP tem 8 dígitos mas não existe,
+        // ela não retorna um erro HTTP (não cai no .catch)
 }
